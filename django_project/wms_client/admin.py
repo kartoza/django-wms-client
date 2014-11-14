4c56ac63c9d2ad567972105e0b4a690b8b9436b8
Tim Sutton <tim@linfiniti.com>
2014-11-14 15:47:58 +0200

    Added missing static resources and migrations for tweaks to model changes

--- django_project/wms_client/admin.py
+++ django_project/wms_client/admin.py
@@ -8,8 +8,8 @@ from wms_client.models.wms_resource import WMSResource
 class WMSResourceAdmin(admin.ModelAdmin):
     """Admin Class for WMSResource Model."""
     exclude = ('slug',)
-    list_display = ('name', 'uri', 'descriptions')
-    list_filter = ['name']
-    search_fields = ['name']
+    list_display = ('name', 'uri')
+    list_filter = ['name', 'uri']
+    search_fields = ['name', 'description']
 
 admin.site.register(WMSResource, WMSResourceAdmin)
