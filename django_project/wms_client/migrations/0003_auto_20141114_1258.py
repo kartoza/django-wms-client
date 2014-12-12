e1baa348e42e00df925c5ad03578d5d55faa46bb
pwhipp <paul.whipp@gmail.com>
2014-12-12 15:16:36 +1000

    Removed preserve_default flag (we're using django 1.6)

--- django_project/wms_client/migrations/0003_auto_20141114_1258.py
+++ django_project/wms_client/migrations/0003_auto_20141114_1258.py
@@ -15,42 +15,35 @@ class Migration(migrations.Migration):
             model_name='wmsresource',
             name='east',
             field=models.FloatField(default=0),
-            preserve_default=False,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='max_zoom',
             field=models.IntegerField(default=19),
-            preserve_default=True,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='min_zoom',
             field=models.IntegerField(default=0),
-            preserve_default=False,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='north',
             field=models.FloatField(default=0),
-            preserve_default=False,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='south',
             field=models.FloatField(default=0),
-            preserve_default=False,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='west',
             field=models.FloatField(default=0),
-            preserve_default=False,
         ),
         migrations.AddField(
             model_name='wmsresource',
             name='zoom',
             field=models.IntegerField(default=0),
-            preserve_default=False,
         ),
     ]
